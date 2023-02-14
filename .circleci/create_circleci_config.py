35520d001e7cfaf687e814901a9dab4c8652d2b9
ydshieh <ydshieh@users.noreply.github.com>
2023-02-14 09:24:44 +0100

    debuggggggggggggggggggggg

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -159,7 +159,7 @@ class CircleCIJob:
         test_command += " | tee tests_output.txt"
 
         test_command = "python -m pytest -n 3 --max-worker-restart=0 --dist=loadfile -s --make-reports=tests_torch tests/models/auto/test_modeling_auto.py::AutoModelTest::test_foo | tee tests_output.txt"
-        test_command = "python3 debug.py"
+        test_command = "python3 debug2.py"
 
         steps.append({"run": {"name": "Run tests", "command": test_command}})
         steps.append({"store_artifacts": {"path": "~/transformers/tests_output.txt"}})
