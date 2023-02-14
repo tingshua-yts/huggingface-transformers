12c02a6feb70067d0bf0be80530136164ba010f3
ydshieh <ydshieh@users.noreply.github.com>
2023-02-14 18:00:15 +0100

    debuggggggggggggggggggggg

--- src/transformers/dynamic_module_utils.py
+++ src/transformers/dynamic_module_utils.py
@@ -145,6 +145,7 @@ def get_class_in_module(class_name, module_path):
     """
     module_path = module_path.replace(os.path.sep, ".")
     module = importlib.import_module(module_path)
+    print(os.listdir("~/.cache/huggingface/modules/transformers_modules/local"))
     return getattr(module, class_name)
 
 
