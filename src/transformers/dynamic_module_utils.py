2b9fcbbb9f0e13fb54b52a9246d1ca55d9e535fb
ydshieh <ydshieh@users.noreply.github.com>
2023-02-16 08:27:01 +0100

    shutil.copy + rm -rf + cp

--- src/transformers/dynamic_module_utils.py
+++ src/transformers/dynamic_module_utils.py
@@ -153,15 +153,15 @@ def get_class_in_module(class_name, module_path):
     module_file_name = module_path.split(os.path.sep)[-1] + ".py"
 
     # copy to a temporary directory
-    # shutil.copy(f"{module_dir}/{module_file_name}", module_dir_backup_temp)
-    os.system(f"cp {module_dir}/{module_file_name} {module_dir_backup_temp}")
+    shutil.copy(f"{module_dir}/{module_file_name}", module_dir_backup_temp)
+    # os.system(f"cp {module_dir}/{module_file_name} {module_dir_backup_temp}")
     # shutil.copyfile(os.path.join(module_dir_backup_temp, module_file_name), os.path.join(module_dir, module_file_name))
 
     # os.remove(f"{module_dir}/{module_file_name}")
     os.system(f"rm -rf {module_dir}/{module_file_name}")
 
-    shutil.copy(os.path.join(module_dir_backup_temp, module_file_name), module_dir)
-    # OK [with shutil.copy or with shutil.copyfile above]: os.system(f"cp {module_dir_backup_temp}/{module_file_name} {module_dir}")
+    # shutil.copy(os.path.join(module_dir_backup_temp, module_file_name), module_dir)
+    os.system(f"cp {module_dir_backup_temp}/{module_file_name} {module_dir}")
     # OK shutil.copyfile(os.path.join(module_dir_backup_temp, module_file_name), os.path.join(module_dir, module_file_name))
 
     # import the module
